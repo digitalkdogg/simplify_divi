8ad754e7a3297df4900b87fccb166c9de0e70404
digitalkdogg <digitalkdogg@gmail.com>
2021-09-26 13:24:00 -0600

    add message to send mail call

--- new-employee.php
+++ new-employee.php
@@ -142,7 +142,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
                             '<br />Start Date : ' . $_POST['start_date'] .
                             '<br />Salary :'. $_POST['pay']  .
                             '<br />benefits : ' . $_POST['beneftis'];
-                            if (send_to_email(get_post_custom_values('send_to_email')) ) {
+                            if (send_to_email(get_post_custom_values('send_to_email'), $message) ) {
                              ?>
                                     You form was uploaded successfully
                                     <p>Do you need to upload a document for this employee</p>
