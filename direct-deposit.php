503fc1c156a9c0e821802728d5d84216f3136fa9
digitalkdogg <digitalkdogg@gmail.com>
2021-10-03 13:09:08 -0600

    add email to the message for direct deposit

--- direct-deposit.php
+++ direct-deposit.php
@@ -168,6 +168,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
                                                               Employee Name : ' . $_POST['employee_name'] .
                                                               '<br />Checking : ' . $_POST['checking'] .
                                                               '<br />Savings : ' . $_POST['savings'] .
+                                                              '<br />Email : ' . $_POST['email_name'] .
                                                               '<br />Routing No :'. $_POST['routing_number']  .
                                                               '<br />Account No : ' . $_POST['account_number'] .
                                                               '<br />Check Image : ';
